3cf23c5a227b6a44c25a4edaa3dea7cfc57f2234
milene08 <milene.souza@aluno.faculdadeimpacta.com.br>
2024-10-22 11:05:14 -0300

    Update escola.py

--- escola.py
+++ escola.py
@@ -1,4 +1,3 @@
-#classe alunos
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base
 
@@ -19,11 +18,22 @@ class Aluno(Base):
     def __repr__(self):
         return f'<Aluno(nome={self.nome}, idade={self.idade})>'
 
-# Criação da tabela
+
+class Turma(Base):
+    __tablename__ = 'turmas'
+
+    id = Column(Integer, primary_key=True)  # A coluna id
+    ano = Column(String)                     # ano (ex: '7')
+    classe = Column(String)                  # letra da turma (ex: 'A')
+
+    def __repr__(self):
+        return f'<Turma(ano={self.ano}, classe={self.classe})>'
+
+# Criação das tabelas
 Base.metadata.create_all(engine)
 
 def adicionar_aluno(nome, idade):
-    novo_aluno = Aluno(nome=nome, idade=idade)  # Corrigido para receber nome
+    novo_aluno = Aluno(nome=nome, idade=idade)
     session.add(novo_aluno)
     session.commit()
     print(f'Aluno {nome} (idade: {idade}) adicionado com sucesso!')
@@ -31,28 +41,48 @@ def adicionar_aluno(nome, idade):
 def consultar_alunos():
     alunos = session.query(Aluno).all()
     for aluno in alunos:
-        print(f'Nome: {aluno.nome}, Idade: {aluno.idade}')  # Impressão formatada
+        print(f'Nome: {aluno.nome}, Idade: {aluno.idade}')
+
+def adicionar_turma(ano, classe):
+    nova_turma = Turma(ano=ano, classe=classe)
+    session.add(nova_turma)
+    session.commit()
+    print(f'Turma {ano} (classe: {classe}) adicionada com sucesso!')
+
+def consultar_turmas():
+    turmas = session.query(Turma).all()
+    for turma in turmas:
+        print(f'Ano: {turma.ano}, Classe: {turma.classe}')  # Correção aqui
 
 def main():
     while True:
         print('\nEscolha uma opção:')
         print('1. Adicionar Aluno')
         print('2. Consultar Alunos')
-        print('3. Sair')
+        print('3. Adicionar Turma')
+        print('4. Consultar Turmas')
+        print('5. Sair')
 
         opcao = input('Opção: ')
         if opcao == '1':
             nome = input('Nome do Aluno: ')
-            idade = int(input('Idade do Aluno: '))  # Solicita a idade do aluno
-            adicionar_aluno(nome, idade)  # Passa o nome e a idade
+            idade = int(input('Idade do Aluno: '))
+            adicionar_aluno(nome, idade)
         elif opcao == '2':
             print("Lista de Alunos:")
             consultar_alunos()
         elif opcao == '3':
-           break
+            ano = input('Ano da Turma: ')
+            classe = input('Classe da Turma: ')
+            adicionar_turma(ano, classe)
+        elif opcao == '4':
+            print("Lista de Turmas:")
+            consultar_turmas()
+        elif opcao == '5':
+            break
         else:
             print("Opção inválida. Tente novamente.")
 
 if __name__ == "__main__":
     main()
-
+]
